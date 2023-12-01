4db3b97b7e18ef0e0fa1b973d52bf08913ba3eca
David Reguera <birrick@gmail.com>
2023-12-01 06:05:38 +0100

    Create a test for the generation of random variates

---
+++ cpp/grid-algorithm/src/tests.cu
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+#include <cuda_runtime.h>
+#include <curand.h>
+#include <curand_kernel.h>
+
+#include "../inc/normal_kernel.h"
+
+
+class CUDATest : public ::testing::Test {
+protected:
+    // Set up any common resources or configurations needed for the tests
+    // For CUDA, you might allocate device memory, etc.
+    void SetUp() override {
+        // Add setup code here if needed
+    }
+
+    // Clean up any resources allocated in SetUp
+    void TearDown() override {
+        // Add cleanup code here if needed
+    }
+};
+
+// Test case for the CUDA function
+TEST_F(CUDATest, AddFunctionTest) {
+    unsigned int numElements = 3;
+    curandState *devStates;
+    float *devResults;
+
+    // Allocate Memory
+    cudaMallocManaged(&devStates, numElements * sizeof(curandState));
+    cudaMallocManaged(&devResults, numElements * sizeof(float));
+
+    // Call the CUDA function
+    generate_normal_kernel<<<1, 1>>>(devStates, numElements, devResults);
+
+    // Copy the result back to the host
+    cudaDeviceSynchronize();
+
+    // Perform assertions to check if the CUDA function worked as expected
+    EXPECT_NEAR(devResults[0], 0.00459315, 1e-5);
+
+    // Free memory
+    cudaFree(devStates);
+    cudaFree(devResults);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
